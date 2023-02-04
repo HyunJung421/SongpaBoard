87966c0dae2d70379823b4da497d4eea75129cf5
HyunJung421 <hyunj8764@gamil.com>
2023-02-04 20:33:17 +0900

    First Commit

---
+++ src/test/java/com/sonpagu/board/SongpaguBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonpagu.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongpaguBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
