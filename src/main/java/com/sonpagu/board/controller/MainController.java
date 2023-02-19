829e04cbcdb18dafa73c1b4e39cd65d4d37af550
HyunJung421 <hyunj8764@gmail.com>
2023-02-19 19:04:22 +0900

    logback 적용
    logback 적용

--- src/main/java/com/sonpagu/board/controller/MainController.java
+++ src/main/java/com/sonpagu/board/controller/MainController.java
@@ -1,16 +1,20 @@
 package com.sonpagu.board.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class MainController {
 
 	@GetMapping("hello")
 	public List<String> hello() {
+		
+		log.info("==== 첫 페이지 로딩 성공 ====");
 		return Arrays.asList("안녕하세요 테스트입니다.", "Hello");
 	}
 }
