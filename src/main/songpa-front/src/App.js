c56ad3e5335ed66e6de22ebf66a415ddccbea906
taetaeyo <op_o@naver.com>
2023-08-21 19:59:25 +0900

    Update App.js
    git test입니다.

--- src/main/songpa-front/src/App.js
+++ src/main/songpa-front/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 function App() {
     const [message, setMessage] = useState([]);
@@ -18,7 +18,7 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
+                <img src={logo} className="App-logo" alt="logo" />
                 <p>
                     Edit <code>src/App.js</code> and save to reload.
                 </p>
